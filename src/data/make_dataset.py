2710653d76458f2eeefd6172a0c14900d1766677
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2020-05-07 18:56:19 -0500

    new function to grab thread details

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,31 +10,29 @@ from pathlib import Path
 
 # Get credentials from DEFAULT instance in praw.ini
 
+
 class Scraper:
 
     # Checkpoint defines savepoints (save everytime n number of comments are collected).
-    def __init__(self, sub_reddit, checkpoint=None, minimum=None):
+    def __init__(self, args):
         self.exectime = time.time()
         self.ids = []
-        self.checkpoint = checkpoint
-        self.interval = checkpoint
-        self.minimum = minimum
-        # By default, save everytime 10000 new comments are collected.
-        if self.checkpoint is None:
-            self.checkpoint = 10000
-            self.interval = 10000
-        if self.minimum is None:
-            self.minimum = 200000
+        self.checkpoint = args.checkpoint
+        self.interval = args.checkpoint
+        self.minimum = args.minimum
         if self.minimum < self.checkpoint:
-            print("The minimum number (default: 200k) of records has to larger than checkpoint (default: 10k)")
+            print(
+                "The minimum number (default: 200k) of records has to larger than checkpoint (default: 10k)")
             sys.exit()
+        self.threaddata = {"thread_ids": [], "title": [], "author_ids": [
+        ], "upvotes": [], "gildings": [], "created_utc": [], "premium" : [], "num_comments" : []}
         self.commentdata = {"comment_body": [],
                             "upvotes": [], "comment_ids": [], "author_ids": []}
-        self.gildings = {"comment_ids": [], "gilds": []}
+        self.gildings = {"comment_ids": [], "gildings": []}
         self.author = {"author_ids": [], "comment_karma": [],
-                       "link_karma": [], "created_utc": [], "is_premium": [], "comment_ids" : [], "created_utc": [], "edited" : []}
+                       "link_karma": [], "created_utc": [], "is_premium": [], "comment_ids": [], "created_utc": [], "edited": []}
         self.reddit = praw.Reddit()
-        self.subreddit = self.reddit.subreddit(sub_reddit).top(limit=None)
+        self.subreddit = self.reddit.subreddit(args.subreddit).top(limit=None)
         self.savepath = "../../data/raw/"
 
     def output_filename(self):
@@ -42,9 +40,9 @@ class Scraper:
         right filepath for saving files.
         Also creates the save folder if it doesn't already exist"""
         dirname = os.path.dirname(__file__)
-        Path(os.path.join(dirname, self.savepath)).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(dirname, self.savepath)).mkdir(
+            parents=True, exist_ok=True)
         return os.path.join(dirname, self.savepath)
-
 
     def retrieveUser(self, comment):
         """Takes in comment object as a parameter, which is used to obtain author information such as:
@@ -61,8 +59,7 @@ class Scraper:
 
     def retrieveGilds(self, comment):
         self.gildings["comment_ids"].append(comment.id)
-        self.gildings["gilds"].append(comment.gildings)
-
+        self.gildings["gildings"].append(comment.gildings)
 
     def retrieveComment(self, comment):
         """Takes in comment object as a parameter, which is used to obtain comment id, comment body
@@ -71,7 +68,7 @@ class Scraper:
         # Comment_id is used as the shared column for merging comment and gild data.
 
         Comment object is passed to parseUser function to obtain author information.
-        
+
         Note that some suspended users will still be retrieved (i.e. their comments will be added, however
         their accounts won't have suspended attribute, but will return a 404 error). We will have to remove
         such comments if the author information cannot be found from authors data table.
@@ -92,9 +89,9 @@ class Scraper:
             else:
                 try:
                     self.retrieveUser(comment)
-                except: 
-                    #Exception as e:
-                    #print(e)
+                except:
+                    # Exception as e:
+                    # print(e)
                     return None
             self.commentdata["comment_ids"].append(comment.id)
             self.commentdata["comment_body"].append(comment.body)
@@ -105,7 +102,16 @@ class Scraper:
             self.commentdata["edited"].append(comment.edited)
             if comment.gildings:
                 self.retrieveGilds(comment)
-                
+
+    def retrieveThread(self, submission):
+        self.threaddata["author_ids"].append(submission.author_fullname[3:])
+        self.threaddata["title"].append(submission.title)
+        self.threaddata["upvotes"].append(submission.ups)
+        self.threaddata["edited"].append(submission.edited)
+        self.threaddata["gildings"].append(submission.gildings)
+        self.threaddata["created_utc"].append(submission.created_utc)
+        self.threaddata["num_comments"].append(submission.num_comments)
+        self.threaddata["premium"].append(submission.author_premium)
 
     def addTo(self):
         """Takes the subreddit input from user as a parameter, calls respective method for retrieving relevant data."""
@@ -120,76 +126,76 @@ class Scraper:
                     self.retrieveComment(comment)
                 self.save_files()
 
-
     def exit_conditions(self):
         """Prompts the user for exit conditions. Enter N and a new minimum in the following prompt to continue scraping."""
 
         flag = True
         while (flag):
-            val = input("Would you like to exit now? Press Y/N; N allows to continue scraping.\n")
+            val = input(
+                "Would you like to exit now? Press Y/N; N allows to continue scraping.\n")
             if (val.lower() == 'y'):
                 sys.exit()
             elif (val.lower() == 'n'):
                 print(f"Current minimum is {self.minimum}\n")
                 try:
-                    new_min_val = int(input("Type in a new minimum (larger than the previous):"))
-                    if new_min_val> self.minimum:
+                    new_min_val = int(
+                        input("Type in a new minimum (larger than the previous):"))
+                    if new_min_val > self.minimum:
                         self.minimum = new_min_val
                         flag = False
                     else:
-                        print("New minimum is smaller than previous minimum. Try again\n")
+                        print(
+                            "New minimum is smaller than previous minimum. Try again\n")
                 except:
                     print("Wrong type of input. Please enter valid input.\n")
                     continue
             else:
                 print("Wrong type of input. Please enter valid input.\n")
 
-
     def save_files(self):
         """Saves the files for every checkpoints, exits the program when minimum number of records is reached"""
-        length  = len(self.commentdata["comment_ids"])
+        length = len(self.commentdata["comment_ids"])
         if (length < self.checkpoint):
             pass
         else:
             t = time.localtime()
             current_time = time.strftime("%H:%M:%S", t)
-            print("Collected {} records so far; Saving in progress. Time now: {}".format(length, current_time))
+            print("Collected {} records so far; Saving in progress. Time now: {}".format(
+                length, current_time))
             data_f = pd.DataFrame(self.commentdata)
             authors_f = pd.DataFrame(self.author)
             gildings_f = pd.DataFrame(self.gildings)
-            data_f.to_csv(f"{self.output_filename()}/comment_data.csv", mode="w", index=False)
-            authors_f.to_csv(f"{self.output_filename()}/author_data.csv", mode="w", index=False)
-            gildings_f.to_csv(f"{self.output_filename()}/gildings_data.csv", mode="w", index=False)
+            data_f.to_csv(
+                f"{self.output_filename()}/comment_data.csv", mode="w", index=False)
+            authors_f.to_csv(
+                f"{self.output_filename()}/author_data.csv", mode="w", index=False)
+            gildings_f.to_csv(
+                f"{self.output_filename()}/gildings_data.csv", mode="w", index=False)
             self.checkpoint += self.interval
         if (length > self.minimum):
-            exectime = ((time.time() - self.exectime ) / (60*60))
-            print("Collected {} records so far; Total execution time: {} hours".format(length, exectime))
+            exectime = ((time.time() - self.exectime) / (60*60))
+            print("Collected {} records so far; Total execution time: {} hours".format(
+                length, exectime))
             self.exit_conditions()
-            
+
 
 def build_parser():
+    MINIMUM = 200000
+    CHECKPOINT = 10000
     parser = argparse.ArgumentParser()
-    parser.add_argument("sub_reddit", help="Specify the subreddit to scrape from")
+    parser.add_argument(
+        "subreddit", help="Specify the subreddit to scrape from")
     parser.add_argument("-m", "--minimum", help="Specify the minimum number of data records to collect",
-                        type=int)
+                        type=int, default=MINIMUM)
     parser.add_argument("-c", "--checkpoint",
-                        help="Save the file every c comments", type=int)
+                        help="Save the file every c comments", type=int, default=CHECKPOINT)
     return parser
-
-                        
 
 
 def main():
     parser = build_parser()
     args = parser.parse_args()
-    if (args.minimum and args.checkpoint):
-        Scraper(args.sub_reddit, checkpoint=args.checkpoint,
-                 minimum=args.minimum).addTo()
-    if (args.minimum):
-        Scraper(args.sub_reddit, minimum=args.minimum).addTo()
-    if (args.checkpoint):
-        Scraper(args.sub_reddit, checkpoint=args.checkpoint).addTo()
-    Scraper(args.sub_reddit).addTo()
+    Scraper(args).addTo()
 
 
 if __name__ == "__main__":
