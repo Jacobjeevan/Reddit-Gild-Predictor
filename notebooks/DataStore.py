f2b4d4f2047ec3f4e3735d8006c194adf631f42a
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-05-26 19:42:03 -0500

    refactored logistic; adding other class files

---
+++ notebooks/DataStore.py
@@ -0,0 +1,80 @@
+#! usr/bin/env python3
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from utils import filter_add_attributes
+
+class DataStore:
+    
+    train_data = pd.read_csv("train_data_baseline.csv")
+    test_data = pd.read_csv("test_data_baseline.csv")
+    
+    def __init__(self):
+        self.xTrain = None
+        self.yTrain = None
+        self.xTest = None
+        self.yTest = None
+        self.OriginalxTrain = None
+        self.OriginalyTrain = None
+        self.transformSet()
+        
+    def __call__(self):
+        return self.getxTrain(), self.getyTrain(), self.getxTest(), self.getyTest()
+ 
+    def createPipeline(self):
+        pipeline = Pipeline([
+            ('filter_add', filter_add_attributes()),
+            ('scaler', StandardScaler()),
+        ])
+        return pipeline
+    
+    def setxTrain(self, x):
+        self.xTrain = x
+        
+    def setyTrain(self, y):
+        self.yTrain = y
+        
+    def revertToOriginal(self):
+        self.xTrain = self.OriginalxTrain
+        self.yTrain = self.OriginalyTrain
+        
+    def setOriginalxTrain(self, x):
+        self.OriginalxTrain = x
+        self.setxTrain(x)
+        
+    def setOriginalyTrain(self, y):
+        self.OriginalyTrain = y
+        self.setyTrain(y)
+        
+    def setxTest(self, x):
+        self.xTest = x
+        
+    def setyTest(self, y):
+        self.yTest = y
+    
+    def getxTrain(self):
+        return self.xTrain
+        
+    def getyTrain(self):
+        return self.yTrain
+        
+    def getxTest(self):
+        return self.xTest
+        
+    def getyTest(self):
+        return self.yTest
+    
+    def setAll(self, xTrain, yTrain, xTest, yTest):
+        self.setOriginalxTrain(xTrain)
+        self.setOriginalyTrain(yTrain)
+        self.setxTest(xTest)
+        self.setyTest(yTest)
+        
+    def transformSet(self):
+        pipeline = self.createPipeline()
+        xTrain = pipeline.fit_transform(DataStore.train_data)
+        yTrain = DataStore.train_data["gildings"].to_list()
+        xTest = pipeline.transform(DataStore.test_data)
+        yTest = DataStore.test_data["gildings"].to_list()
+        self.setAll(xTrain, yTrain, xTest, yTest)
+        
---
+++ notebooks/utils.py
@@ -0,0 +1,40 @@
+#! usr/bin/env python3
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import make_scorer
+from sklearn.metrics import confusion_matrix
+from sklearn.base import BaseEstimator, TransformerMixin
+
+def getTruePositives(y_true, y_pred): 
+    return confusion_matrix(y_true, y_pred)[1, 1]
+
+def getFalsePositives(y_true, y_pred): 
+    return confusion_matrix(y_true, y_pred)[0, 1]
+
+def getFalseNegatives(y_true, y_pred): 
+    return confusion_matrix(y_true, y_pred)[1, 0]
+
+scoring = {'truePositives': make_scorer(getTruePositives), 'falsePositives': make_scorer(getFalsePositives), 
+       'falseNegatives': make_scorer(getFalseNegatives), 'roc_auc': make_scorer(roc_auc_score)}
+
+def getScoring():
+    return scoring
+
+class filter_add_attributes(BaseEstimator, TransformerMixin):
+    '''Custom transformer based on Sklearn's classes.
+    Takes in dataframe (train or test) and adds new features and returns
+    a filtered version of the original train/test datasets.'''
+    def fit(self, X, y=None):
+        return self.fit_transform(X)
+    def transform(self, X, y=None):
+        return self.fit_transform(X)
+    def fit_transform(self, X, y=None):
+        '''Calculates and adds comment body length and account activity (based on frequency of comment author)
+        as features. Returns a new dataframe with the added columns.'''
+        data = X.copy()
+        data["comment_length"] = data.comment_body.apply(lambda x: len(x))
+        data["account_activity"] = data.author_ids.map(data.author_ids.value_counts())
+        data["is_premium"] = data.is_premium.astype(int)
+        items = ["ups", "comment_karma", "link_karma", "is_premium", "comment_age_days", "acc_age_days", "comment_length", 
+                 "account_activity"]
+        return data.filter(items=items, axis=1)
